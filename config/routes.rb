9181443e936da3650315a9478292635adeb54960
Alex L <getshiftshark@gmail.com>
2013-05-28 18:50:30 +0100

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 TestApp::Application.routes.draw do
+
+resources :users
+root to: 'static_pages#home'
+
 match '/home', to: 'static_pages#home', via: 'get'
-
 match '/contact', to: 'static_pages#contact', via: 'get'
+match '/signup', to: 'users#new'
+  
 
-  root to: 'static_pages#home'
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
