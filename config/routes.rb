250020f0fdf090964329e81848f6d9613bab2678
Alex L <getshiftshark@gmail.com>
2013-05-28 11:33:12 +0100

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 TestApp::Application.routes.draw do
-  get "static_pages/home"
+match '/home', to: 'static_pages#home', via: 'get'
 
-  get "static_pages/contact"
+match '/contact', to: 'static_pages#contact', via: 'get'
+
+  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
