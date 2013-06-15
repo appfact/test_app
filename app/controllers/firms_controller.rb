c00794e1fd0ab40a5098dfa6577143beee54113e
Alex L <getshiftshark@gmail.com>
2013-06-15 10:52:30 +0100

    1506 sat morning

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -52,9 +52,10 @@ def create
 
   def shiftrequests
     @firm = Firm.find(params[:id])
-    @shift_requests_items = @firm.shift_requests.find(:all, 
-          conditions: ["worker_status is ? AND manager_status is ? 
-                         AND start_datetime > ?", true, nil, Time.now.to_datetime], order: [:shift_id, :created_at])
+    @shift_requests_items = @firm.shift_requests
+                                .where(:worker_status => true, :manager_status => nil) 
+                                .where(':start_datetime > ?', Time.now.to_datetime)
+                                .order(:shift_id, :created_at)
   end
 
   def newshift
